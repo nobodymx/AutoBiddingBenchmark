75cb8b4a9023718d7e43843d02f9e34b7f70ec60
nobodymx <65476805+nobodymx@users.noreply.github.com>
2024-04-14 14:15:37 +0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from DataLogger.datalogger import DataLogger
-from Config.generate_log_data_config import config
-
-
-def run():
-    datalogger = DataLogger(config["basic_config"]["num_agents"])
-    datalogger.load()
-
-
